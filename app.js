2762d89bfb2c075f8d8121fe5ca5388449d9e86e
Angelo Zorn <52333277+angelozorn@users.noreply.github.com>
2021-12-25 12:41:06 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const navBar = document.querySelector("#navBar");
 
+document.onload = addAnimations();
+
+function addAnimations() {
+        navBar.classList.toggle("sticky-top");
+  
+  function stickyTop(){
+        navBar.classList.toggle("sticky-top");
+}
